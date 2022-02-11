b0c8c613724f7dcb6d75785efd6518265eeadbfe
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-02-11 06:53:26 +0500

    Create a new class for DBcontext to relationship between database

---
+++ TrevalApI/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace TrevalApI.Data
+{
+    public class ApplicationDbContext: IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> Option):base(Option)
+        {
+
+        }
+
+
+    }
+}
